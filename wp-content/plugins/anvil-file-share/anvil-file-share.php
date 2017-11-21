7b6e80d3ed5b2aca8083d9abe8189e773d75f5d8
Austin Verburg <dairyboycomics@gmail.com>
2017-11-20 19:45:16 -0800

    added file uploader post type

--- wp-content/plugins/anvil-file-share/anvil-file-share.php
+++ wp-content/plugins/anvil-file-share/anvil-file-share.php
@@ -15,5 +15,40 @@ function add_pepekura_files($addPep){
 }
 add_filter('upload_mimes', 'add_pepekura_files', 1, 1);
 
+function create_file_share() {
+    $labels = array(
+        'name'                => __( 'Files' ),
+        'singular_name'       => __( 'File' ),
+        'menu_name'           => __( 'Files' ),
+        'all_items'           => __( 'All Files' ),
+        'view_item'           => __( 'View File' ),
+        'add_new_item'        => __( 'Add New File' ),
+        'add_new'             => __( 'Add New' ),
+        'edit_item'           => __( 'Edit File' ),
+        'update_item'         => __( 'Update File' ),
+        'search_items'        => __( 'Search File' )
+    );
+
+
+    $args = array(
+      'label' => 'Files',
+      'labels' => $labels,
+        'public' => true,
+        'show_ui' => true,
+        'capability_type' => 'post',
+        'has_archive' => true,
+        'hierarchical' => false,
+        'rewrite' => array('slug' => 'property'),
+        'query_var' => true,
+        'menu_icon' => 'dashicons-building',
+        'supports' => array(
+            'title',
+            'editor',
+            'thumbnail'),
+        );
+    register_post_type( 'files', $args );
+}
+add_action( 'init', 'create_file_share' );
+
 
 ?>
